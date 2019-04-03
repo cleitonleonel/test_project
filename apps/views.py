d5a300d9361e0a2616477899835c9a0702533bf4
Adenildo Junior <adenildojunior52@gmail.com>
2019-04-03 07:39:41 -0300

    Algumas pequenas correções de bugs e situações que poderiam a se tornar problemas futuramente.
    {'task': ['t11', 't28'], 'status': 'ready'}

--- apps/views.py
+++ apps/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
-    return render(request, "base_page.html", {'base_page': 'base_page.html'})
+    return render(request, "blank_page.html", {'base_page': 'base_page.html'})
--- test_project/settings.py
+++ test_project/settings.py
@@ -118,5 +118,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/static/'
-STATICFILES_DIRS = [ BASE_DIR+os.path.join('/static'), ]
+STATIC_ROOT = STATIC_URL
+STATICFILES_DIRS = [BASE_DIR+os.path.join('/static'), ]
