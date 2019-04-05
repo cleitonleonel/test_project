8f5f12f9921c920784441652596cdbe8e6d04b92
Adenildo Junior <adenildojunior52@gmail.com>
2019-04-05 16:25:03 -0300

    Subindo mudanças para sincronizar com o resto das funcionalidades. Vou mudar de branch
    {'task': 't28', 'status': 'reviewing'}

--- static/js/components/input-components.js
+++ static/js/components/input-components.js
@@ -1,86 +1,18 @@
-Vue.component('app-field', {
-	props: ['id', 'value', 'label', 'error', 'placeholder', 'title', 'type','required', 'classes'],
-  data: function () {
-    return {
-    }
-  },
-  template:
-    `
-  	<div class=''>
-  		<label v-if="label!=''" style='font-weight: normal;margin-left:4px;'><sub>{{ label }}</sub></label>
-  		<app-raw-input
-  		 class="form-control"
-       :required="required"
-       :id="id"
-       :value="value"
-       @input="$emit('input', $event.target.value)"
-       :class="{'wrong_field': error, classes}"
-       :placeholder="placeholder"
-       :classes="classes"
-       :title="title"
-       :type="type"></app-raw-input>
-  		<label v-if='error' class="label_error">{{ error }}</label>
-		</div>
-    `
-});
-
-Vue.component('app-input-group',{
-  props:['type', 'id', 'error', 'title','placeholder', 'classes', 'required','object'],
+Vue.component('app-input', {
+  props:['type', 'id', 'title', 'placeholder', 'classes', 'value'],
   template:
   `
-  <div>
-    <div class="input-group mb-3">
-      <div class="input-group-prepend">
-        <span class="input-group-text round-border" id="basic-addon1"><i :class="[, classes]"></i></span>
-      </div>
-      <app-raw-input
-       :type="type"
-       :id="id"
-       :title="title"
-       :placeholder="placeholder"      
-       classes="round-border" 
-       :required="required"
-       :object="object"
-       aria-describedby="basic-addon1"></app-raw-input>
-    </div>
-  </div>
+  <input :type="type" :id="id" :title="title" :placeholder="placeholder" :class="classes" :value="value" @input="$emit('input', $event.target.value)" class="form-control">
   `
-});
-
-Vue.component('app-raw-input',{
-  props:
-      ['id',
-        'value',
-        'error',
-        'placeholder',
-        'title',
-        'type',
-        'required',
-        'classes',
-        'vmodel'
-      ],
-  template:
-  `
-  <input
-   class="form-control"
-   :required="required"
-   :id="id"
-   :value="value"
-   @input="$emit('input', $event.target.value)"
-   :class="[{ 'wrong_field': error }, classes]"
-   :placeholder="placeholder"
-   :title="title"
-   :type="type"
-   v-model="vmodel">
-  `
+  ,
 });
 
 Vue.component('app-submit-button',{
   props: ['otma_login'],
   template:
       `
-<div>
-  <button type="submit" class="btn btn-sm submit-button round-border login-header" @click="otma_login()" style="margin-top: 10px;">ENVIAR</button>
-</div>
-`
+    <div>
+      <button type="submit" class="btn btn-sm submit-button round-border login-header" @click="otma_login()" style="margin-top: 10px;">ENVIAR</button>
+    </div>
+    `
 });
--- test_project/settings.py
+++ test_project/settings.py
@@ -156,6 +156,7 @@ BOWER_INSTALLED_APPS = (
     'jquery',
     'font-awesome',
     'jquery-nicescroll',
+    'google-fonts',
     #https://github.com/snokier/v-contextmenu
     #'vue-resize-mixin',
     #'rateyo'
