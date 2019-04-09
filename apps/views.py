c7dacf2e1f05ae04bb1faffb8179f2cbe89e482e
Adenildo Junior <adenildojunior52@gmail.com>
2019-04-09 11:34:47 -0300

    Corrigidos problemas na base_page e adicionados alguns arquivos para a PEP8 {'task': 't30', 'status': 'ready'}

--- apps/views.py
+++ apps/views.py
@@ -3,12 +3,15 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, "blank_page.html", {'base_page': 'base_page.html'})
+    return render(request, "blank_page.html",
+                  {'base_page': 'base_page.html'})
 
 
 def login(request):
-    return render(request, "core/authentication/new_login.html", {'base_page': 'core/authentication/new_base_auth.html'})
+    return render(request, "core/authentication/new_login.html",
+                  {'base_page': 'core/authentication/new_base_auth.html'})
+
 
 def signup(request):
-    return render(request, "core/authentication/new_base_auth.html", {'base_page': 'core/authentication/new_base_auth.html'})
-
+    return render(request, "core/authentication/new_base_auth.html",
+                  {'base_page': 'core/authentication/new_base_auth.html'})
