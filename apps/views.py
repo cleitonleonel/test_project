a4e74a915f3c50740f079fc8cc771189f89923c8
Cleiton Leonel <cleiton.leonel@gmail.com>
2019-04-10 11:36:46 -0300

    Terminamos a tarefa de validação de logout...
    {'task':'T38', 'status':'ready'}

--- apps/views.py
+++ apps/views.py
@@ -1,6 +1,8 @@
 from django.contrib.auth import logout
 from django.shortcuts import render, redirect
 
+from apps.project.management.actions.validators import check_islogged
+
 
 def index(request):
     return render(request, "blank_page.html",{'base_page': 'base_page.html'})
@@ -15,5 +17,8 @@ def signup(request):
 
 
 def logout_page(request):
-    logout(request)
-    return redirect("/login")
+    if check_islogged(request):
+        logout(request)
+        return redirect("/login")
+    else:
+        return render(request, "core/authentication/logout_error.html", {'base_page':'core/authentication/base_authentication.html'})
