4e165b40190c6604a5854e81edbc8b9d734055ce
Diego Pasti <diegopasti@gmail.com>
2019-04-10 09:21:54 -0300

    Revisão de componentes e telas de login e signup.

--- apps/views.py
+++ apps/views.py
@@ -1,17 +1,13 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def index(request):
-    return render(request, "blank_page.html",
-                  {'base_page': 'base_page.html'})
+    return render(request, "blank_page.html",{'base_page': 'base_page.html'})
 
 
 def login(request):
-    return render(request, "core/authentication/new_login.html",
-                  {'base_page': 'core/authentication/new_base_auth.html'})
+    return render(request, "core/authentication/login.html",{'base_page': 'core/authentication/base_authentication.html'})
 
 
 def signup(request):
-    return render(request, "core/authentication/new_base_auth.html",
-                  {'base_page': 'core/authentication/new_base_auth.html'})
+    return render(request, "core/authentication/signup.html",{'base_page': 'core/authentication/base_authentication.html'})
