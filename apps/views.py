5d68a2bc72f21c5b07013a78459f53487313135c
Adenildo Junior <adenildojunior52@gmail.com>
2019-04-10 13:29:13 -0300

    Corrigidos problemas de PEP8. Procurar manter o padrão, para não acumular problemas nos testes.

--- apps/views.py
+++ apps/views.py
@@ -5,15 +5,17 @@ from apps.project.management.actions.validators import check_islogged
 
 
 def index(request):
-    return render(request, "blank_page.html",{'base_page': 'base_page.html'})
+    return render(request, "blank_page.html", {'base_page': 'base_page.html'})
 
 
 def login(request):
-    return render(request, "core/authentication/login.html",{'base_page': 'core/authentication/base_authentication.html'})
+    return render(request, "core/authentication/login.html",
+                  {'base_page': 'core/authentication/base_authentication.html'})
 
 
 def signup(request):
-    return render(request, "core/authentication/signup.html",{'base_page': 'core/authentication/base_authentication.html'})
+    return render(request, "core/authentication/signup.html",
+                  {'base_page': 'core/authentication/base_authentication.html'})
 
 
 def logout_page(request):
@@ -21,4 +23,5 @@ def logout_page(request):
         logout(request)
         return redirect("/login")
     else:
-        return render(request, "core/authentication/logout_error.html", {'base_page':'core/authentication/base_authentication.html'})
+        return render(request, "core/authentication/logout_error.html",
+                      {'base_page': 'core/authentication/base_authentication.html'})
