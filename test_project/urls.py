2ccd69b30b6c7c28bed2f785f0f70ec697424add
Adenildo Junior <adenildojunior52@gmail.com>
2019-04-17 17:04:41 -0300

    Revert "F13"

--- test_project/urls.py
+++ test_project/urls.py
@@ -17,18 +17,18 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path, include
 from apps.project.management.actions.api import register_frontend
-from apps.views import index_page, login_page, signup_page, logout_page, change_password_page, entities
+from apps.views import index_page, login_page, signup_page, logout_page
 
-urlpatterns = [    
-    url(r'^$', index_page), 
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    url(r'^$', index_page),
+    # Authentication rotes using the authentication modules resources.
+
     url(r'^login/$', login_page),
     url(r'^signup/$', signup_page),
     url(r'^logout/$', logout_page),
-    url(r'^change_password/$', change_password_page),
-    url(r'^entities/$', entities),
-    url(r'api/(?P<company_repository>\w.+)/(?P<project_name>\w.+)/management/actions/register/frontend$',
-        register_frontend),
-    path('admin/', admin.site.urls),
+    #url(r'^change_password/$', change_password_page),
+    url(r'api/(?P<company_repository>\w.+)/(?P<project_name>\w.+)/management/actions/register/frontend$',register_frontend),
+
     path('api/core/authentication', include('otma.apps.core.authentication.urls')),
-    path('api/entities/', include('otma.apps.entities.urls')),
 ]
