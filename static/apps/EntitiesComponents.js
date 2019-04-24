4bd57fccaaf8a386b242b858db0ca95dea7f894a
Adenildo Junior <adenildojunior52@gmail.com>
2019-04-24 18:00:36 -0300

    Poucas modificações

--- static/apps/EntitiesComponents.js
+++ static/apps/EntitiesComponents.js
@@ -16,6 +16,11 @@ Vue.component('app_entities_table', {
 			scope.form.entity.object.comments = scope.data.objects[key].comments;
 		},
 	},
+	filters: {
+    moment: function (date) {
+      return moment(date).format('DD/MM/YYYY, HH:mm:ss');
+    }
+  },
 	template: `
 		<div>
 			<table :class='classes' style='width:100%;'>
@@ -30,14 +35,14 @@ Vue.component('app_entities_table', {
 					<td>Situação</td>
 					<td>Criação</td>
 					<td>Últ. Alteração</td>
-					<td>#</td>
+					<td width="10%">#</td>
 				</tr>
 
 				<tr v-if='data.controls.loaded==false' style='font-size: 12px;text-align:center;'>
 					<td colspan='11'>Nenhum registro cadastrado.</td>
 				</tr>
 
-				<tr v-if='data.controls.loaded' v-for='(entity, index) in data.objects'>
+				<tr v-if='data.controls.loaded && entity.status != "DISABLED"' v-for='(entity, index) in data.objects'>
 					<td>{{entity.type}}</td>
 					<td>{{entity.official_doc}}</td>
 					<td>{{entity.name}}</td>
@@ -46,14 +51,17 @@ Vue.component('app_entities_table', {
 					<td>{{entity.company_relation}}</td>
 					<td>{{entity.activities}}</td>
 					<td>{{entity.status}}</td>
-					<td>{{entity.creation_date}}</td>
-					<td>{{entity.last_update}}</td>
-					<td style="padding-left: 5px;padding-right: 5px;">
-						<a role="button" @click="edit(index)" class="btn btn-sm btn-primary" style="color: #ffffff;cursor: pointer;">Editar {{index}}</a>
-						<a role="button" @click="edit(index)" data-toggle="modal" data-target="#modalExemplo" class="btn btn-sm btn-primary" style="color: #ffffff;cursor: pointer;margin-top: 5px;">Desativar</a>
+					<td>{{entity.creation_date | moment}}</td>
+					<td>{{entity.last_update | moment}}</td>
+					<td style="padding:5px;">
+						<div class="btn-group btn-group-sm" role="group" aria-label="...">
+						<button @click="edit(index)" type="button" class="btn btn-info">Editar</button>
+						<button @click="edit(index)" type="button" class="btn btn-danger" data-toggle="modal" data-target="#modalExemplo">Desativar</button>
+						</div>
 					</td>
 				</tr>
 			</table>
+			
 		</div>
 	`,
 });
@@ -78,13 +86,13 @@ Vue.component('app_entities_form', {
 		    </div>
 
 		    <div class='col-lg-2 col-md-3 col-sm-3 col-xs-12'>
-		      <app_select classes='form-control' label='Tipo'  v-model='form.object.type' :options='form.options.entitiy_types' title='Informe o tipo da entidade.'></app_select>
+		      <app_select classes='form-control' label='Tipo'  v-model='form.object.type' :options='form.options.entitiy_types' title='Informe o tipo da entidade'></app_select>
 		    </div>
 
 		    <div class='col-lg-2 col-md-6 col-sm-6 col-xs-12'>
 		      <template v-if="form.object.nacionality=='BR'">
-			      <app_field label="CPF" title="Informe o CPF." classes="form-control" v-model="form.object.official_document" v-if="form.object.type== 'PF'"></app_field>
-			      <app_field label="CNPJ" title="Informe o CNPJ." classes="form-control" v-model="form.object.official_document" v-else=""></app_field>
+			      <app_field label="CPF" title="Informe o CPF" classes="form-control" v-model="form.object.official_doc" v-if="form.object.type== 'PF'"></app_field>
+			      <app_field label="CNPJ" title="Informe o CNPJ" classes="form-control" v-model="form.object.official_doc" v-else=""></app_field>
 		      </template>
 
 		      <template v-else>
@@ -93,22 +101,22 @@ Vue.component('app_entities_form', {
 		    </div>
 
 				<div class='col-lg-6 col-md-8 col-sm-12 col-xs-12'>
-		      <app_field classes='form-control' label='Nome completo' :value='form.object.name' title='Informe o nome completo.' v-if="form.object.type== 'PF'"></app_field>
-		      <app_field classes='form-control' label='Razão social' :value='form.object.name' title='Informe a razão social.' v-else></app_field>
+		      <app_field classes='form-control' label='Nome completo' :value='form.object.name' title='Informe o nome completo' v-if="form.object.type== 'PF'"></app_field>
+		      <app_field classes='form-control' label='Razão social' :value='form.object.name' title='Informe a razão social' v-else></app_field>
 		    </div>
 
 		    <div class='col-lg-2 col-md-4 col-sm-7 col-xs-12'>
-		      <app_field classes='form-control' label='Apelido' :value='form.object.popular_name' title='Informe o nome mais comum dessa entidade.' v-if="form.object.type== 'PF'"></app_field>
-		      <app_field classes='form-control' label='Nome fantasia' :value='form.object.popular_name' title='Informe o nome mais comum dessa entidade.' v-else></app_field>
+		      <app_field classes='form-control' label='Apelido' :value='form.object.popular_name' title='Informe o nome mais comum dessa entidade' v-if="form.object.type== 'PF'"></app_field>
+		      <app_field classes='form-control' label='Nome fantasia' :value='form.object.popular_name' title='Informe o nome mais comum dessa entidade' v-else></app_field>
 		    </div>
 
 		    <div class='col-lg-2 col-md-4 col-sm-5 col-xs-12'>
-		      <app_field classes='form-control' label='Data de nascimento' :value='form.object.birthdate_foundation' title='Informe a data de nascimento.' v-if="form.object.type== 'PF'"></app_field>
-		      <app_field classes='form-control' label='Data de fundação' :value='form.object.birthdate_foundation' title='Informe a data de fundação.' v-else></app_field>
+		      <app_field classes='form-control' label='Data de nascimento' :value='form.object.birthdate_foundation' title='Informe a data de nascimento' v-if="form.object.type== 'PF'"></app_field>
+		      <app_field classes='form-control' label='Data de fundação' :value='form.object.birthdate_foundation' title='Informe a data de fundação' v-else></app_field>
 		    </div>
 
 		    <div class='col-lg-4 col-md-4 col-sm-6 col-xs-12'>
-		      <app_select_multiple classes='form-control' label='Tipo de relação'  v-model='form.object.company_relations' :options='form.options.company_relations' empty="Não definido" title='Selecione os tipos de relações que essa entidade tem com a empresa.'></app_select_multiple>
+		      <app_select_multiple classes='form-control' label='Tipo de relação'  v-model='form.object.company_relations' :options='form.options.company_relations' empty="Não definido" title='Selecione os tipos de relações que essa entidade tem com a empresa'></app_select_multiple>
 		    </div>
 
 		    <div class='col-lg-4 col-md-4 col-sm-6 col-xs-12'>
@@ -118,7 +126,7 @@ Vue.component('app_entities_form', {
 
 	    <div class='row'>
 			  <div class='col-lg-12 col-md-12 col-sm-12 col-xs-12'>
-			    <app_textarea classes='form-control' label='Comentário'  v-model='form.object.comments' :options='form.options.comments' title='Preencher com alguma informação ou comentários.'></app_textarea>
+			    <app_textarea classes='form-control' label='Comentário'  v-model='form.object.comments' :options='form.options.comments' title='Preencher com alguma informação ou comentários'></app_textarea>
 		    </div>
 	    </div>
 		</div>
@@ -129,6 +137,21 @@ Vue.component('app_disable_entity', {
 	mixins:[base_controller],
 	props:['data','form'],
 	methods: {
+		clean_form: function(){
+			let scope = this;
+			scope.form.entity.object.index 						= '';
+			scope.form.entity.object.id 							= '';
+			scope.form.entity.object.type 						= 'PJ';
+			scope.form.entity.object.official_doc 		= '';
+			scope.form.entity.object.name 						= '';
+			scope.form.entity.object.popular_name 		= '';
+			scope.form.entity.object.activities 			= '';
+			scope.form.entity.object.company_relation = '';
+			scope.form.entity.object.status 					= '';
+			scope.form.entity.object.nationality 			= 'BR';
+			scope.form.entity.object.comments 				= '';
+		},
+
 		disable: function(index) {
 			let scope = this;
 			let data_parameters = {
@@ -136,16 +159,14 @@ Vue.component('app_disable_entity', {
 				password: scope.form.disable_confirm.password,
 				reason: scope.form.disable_confirm.reason
 			};
-			alert('ve se ta normal:'+data_parameters);
-			alert('olha so que foda essa porra caralho: '+JSON.stringify(data_parameters));
 
 			let success_function = function(response){
+				this.success_notify('Entidade desabilitada com sucesso', '');
 				scope.data.objects.splice(index,1);
-				alert('pelo menos ta chegando aqui mano, podia ser pior')
 			};
 
 			let failure_function = function(response){
-				alert('Desculpa, eu só gosto de você como amigo')
+
 			};
 
 			let validation_function = function(response){
@@ -158,16 +179,22 @@ Vue.component('app_disable_entity', {
 	},
 	template:
 	`
-		<form>
-			<app_input type="password" placeholder="Senha" classes="form-control" v-model="form.disable_confirm.password"></app_input>
-			<app_textarea classes='form-control' label='Motivo'  v-model='form.disable_confirm.reason' title='Qual o motivo de desativar esta entidade?'></app_textarea>
-			</div>
-						<div class="modal-footer">
-							<button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
-							<button type="button" class="btn btn-primary">Salvar mudanças</button>
-						</div>
-			<button type="button" class="btn btn-primary" @click="disable(form.entity.object.index)">Desativar</button>
+	<div class="modal-body">
+		<p style="border: 1px solid #ced4da;background: #f5f5f5; border-radius: 5px;padding: 5px;color: #000;">
+			<span style="font-size: 1.1em;">Atenção!</span>
+			<span style="font-size: 0.8em;">Esta é uma operação de alto risco! Se fizer isto, a entidade não será mais exibida nesta lista! Por isso, pedimos uma confirmação de senha e uma justificativa, que será salva junto com as informações de alteração.</span>
+		</p>
+		<form autocomplete="off">
+			<input autocomplete="false" name="hidden" type="text" style="display:none;">
+			<app_field type="password" label="Senha" classes="form-control" v-model="form.disable_confirm.password"></app_field>
+			<app_textarea classes='form-control' label='Justificativa'  v-model='form.disable_confirm.reason' title='Qual o motivo de desativar esta entidade?'></app_textarea>
 		</form>
+		<div class="modal-footer">
+			<button type="button" class="btn btn-primary" @click="disable(form.entity.object.index)" data-dismiss="modal">Desativar</button>
+		</div>
+	</div>
+			
+		
 	
 	`
 });
@@ -251,7 +278,7 @@ Vue.component('app_entities', {
 	},
 	template: `
 		<div>
-			<app_entities_table :form="forms" :data='data' classes='table-bordered table-hover'></app_entities_table>
+			<app_entities_table :form="forms" :data='data' classes='table-bordered table-hover text-center'></app_entities_table>
 			<br>
 
 			<app_entities_form :form='forms.entity'></app_entities_form>
@@ -260,14 +287,12 @@ Vue.component('app_entities', {
 				<div class="modal-dialog" role="document">
 					<div class="modal-content">
 						<div class="modal-header">
-							<h5 class="modal-title" id="exampleModalLabel">Título do modal</h5>
+							<h5 class="modal-title" id="exampleModalLabel">Desabilitar entidade</h5>
 							<button type="button" class="close" data-dismiss="modal" aria-label="Fechar">
 								<span aria-hidden="true">&times;</span>
 							</button>
 						</div>
-						<div class="modal-body">
-							<app_disable_entity :data='data' :form="forms"></app_disable_entity>
-						
+						<app_disable_entity :data='data' :form="forms"></app_disable_entity>
 					</div>
 				</div>
 			</div>
